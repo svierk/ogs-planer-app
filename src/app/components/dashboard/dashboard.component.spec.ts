eeafc829b358fd8c17d09841df27ec2231952195
Sebastiano Schwarz <s.vierk@hotmail.de>
2023-06-24 20:25:14 +0200

    test(ui): increase coverage for dashboard component

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ExcelService } from 'src/app/services/excel.service';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,9 +9,14 @@ describe('DashboardComponent', () => {
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(() => {
+    const excelService: Partial<ExcelService> = {
+      exportToExcel: jasmine.createSpy('exportToExcel'),
+    };
+
     TestBed.configureTestingModule({
       declarations: [DashboardComponent],
       imports: [MatIconModule, MatListModule],
+      providers: [{ provide: ExcelService, useValue: excelService }],
     });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
@@ -19,5 +25,19 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should export children list', () => {
+    // given
+    component.children = [{ firstName: 'test', lastName: 'child' }];
+    spyOn(component, 'exportChildrenList').and.callThrough();
+    const button = fixture.debugElement.nativeElement.querySelector('mat-list-item');
+
+    // when
+    button.click();
+    fixture.detectChanges();
+
+    // then
+    expect(component.exportChildrenList).toHaveBeenCalledTimes(1);
   });
 });
