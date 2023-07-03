eb8f0417437f72ef6f82563ed7b5cbdf89a9c9f3
Sebastiano Schwarz <s.vierk@hotmail.de>
2023-07-03 15:57:51 +0200

    test(ui): cover children activities dialog create and update actions

--- src/app/components/children-activities-dialog/children-activities-dialog.component.spec.ts
+++ src/app/components/children-activities-dialog/children-activities-dialog.component.spec.ts
@@ -6,13 +6,28 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Child } from 'src/app/models/child';
+import { Course } from 'src/app/models/course';
+import { Days } from 'src/app/models/days';
 import { DbService } from 'src/app/services/db.service';
 import { ChildrenActivitiesDialogComponent } from './children-activities-dialog.component';
 
-const child = {
-  firstName: 'test',
-  lastName: 'child',
-};
+const child: Child = { id: 123, firstName: 'test', lastName: 'child', classId: '123' };
+const courses: Course[] = [
+  {
+    id: 123,
+    name: 'course',
+    teacher: 'teacher',
+    day: Days.Monday,
+    start: 'start',
+    end: 'end',
+  },
+];
+const earlyCare: any[] = [{ id: 123, childId: 123 }];
+const lunch: any[] = [{ id: 123, childId: 123 }];
+const homework: any[] = [{ id: 123, childId: 123 }];
+const childCourses: any[] = [{ id: 123, childId: 123, courseId: 123 }];
+const pickup: any[] = [{ id: 123, childId: 123 }];
 
 describe('ChildrenActivitiesDialogComponent', () => {
   let component: ChildrenActivitiesDialogComponent;
@@ -48,11 +63,73 @@ describe('ChildrenActivitiesDialogComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should initialize the update internal user form as valid', () => {
+  it('should initialize children activities form as valid', () => {
     // given
     component.child = child;
+    component.courses = courses;
+    component.earlyCare = earlyCare;
+    component.lunch = lunch;
+    component.homework = homework;
+    component.childCourses = childCourses;
+    component.pickup = pickup;
 
     // when
+    fixture.detectChanges();
+
+    // then
+    expect(component.activitiesForm.valid).toEqual(true);
+  });
+
+  it('should create children activities', () => {
+    // given
+    spyOn(component.dbService, 'createEarlyCare');
+    spyOn(component.dbService, 'createLunch');
+    spyOn(component.dbService, 'createHomework');
+    spyOn(component.dbService, 'createPickup');
+    spyOn(component.dbService, 'createChildCourses');
+    spyOn(component.dbService, 'getEarlyCare');
+    spyOn(component.dbService, 'getLunch');
+    spyOn(component.dbService, 'getHomework');
+    spyOn(component.dbService, 'getChildCourses');
+    spyOn(component.dbService, 'getPickup');
+    component.child = child;
+    component.courses = courses;
+    component.childCourses = childCourses;
+
+    // when
+    fixture.detectChanges();
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    button.click();
+    fixture.detectChanges();
+
+    // then
+    expect(component).toBeTruthy();
+  });
+
+  it('should update children activities', () => {
+    // given
+    spyOn(component.dbService, 'updateEarlyCare');
+    spyOn(component.dbService, 'updateLunch');
+    spyOn(component.dbService, 'updateHomework');
+    spyOn(component.dbService, 'updatePickup');
+    spyOn(component.dbService, 'updateChildCourses');
+    spyOn(component.dbService, 'getEarlyCare');
+    spyOn(component.dbService, 'getLunch');
+    spyOn(component.dbService, 'getHomework');
+    spyOn(component.dbService, 'getChildCourses');
+    spyOn(component.dbService, 'getPickup');
+    component.child = child;
+    component.courses = courses;
+    component.earlyCare = earlyCare;
+    component.lunch = lunch;
+    component.homework = homework;
+    component.childCourses = childCourses;
+    component.pickup = pickup;
+
+    // when
+    fixture.detectChanges();
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    button.click();
     fixture.detectChanges();
 
     // then
